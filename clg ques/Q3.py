393094ad20fb3991a582e9506dd5fbd2a0dfda68
Vasu Sehgal <44269903+vasusehgal@users.noreply.github.com>
2019-10-01 12:52:42 +0530

    updated question 3
    changed array and its elements

--- clg ques/Q3.py
+++ clg ques/Q3.py
@@ -9,14 +9,14 @@ Created on Sat Aug 10 18:35:49 2019
 @author: SUNNY THAKUR
 """
 
-x = int(input("Enter 1st number "))
-y = int(input("Enter 2nd number " ))
+x = int(input("Enter the 1st number "))
+y = int(input("Enter the 2nd number " ))
 
 def func():
-    arr = [[i*j for j in range(1,y+1)] for i in range(1,x+1) ]
+    a = [[i*j for j in range(1,y+1)] for i in range(1,x+1) ]
 #    for i in range(1,x):
 #        for j in range(1,y):
 #            arr[i][j] = i*j
-    print(arr)
+    print(a)
 
 func()
